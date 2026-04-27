e3fba8b198f4c6cab81d216f7b1ae21b678164af
kumari-kajal123 <kumarikajal.2223k@gmail.com>
2026-04-27 08:42:19 +0530

    Taking Input From User

--- Array/Day1.java
+++ Array/Day1.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Day1{
         public static void main(String[] args) {
-        // Your code here
-        System.out.println("Hello, World!");
+        // TAKING INPUT
+        int number[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+        int phy = sc.nextInt();
+        System.out.println("phy marks : "  + phy);
+        int chem = sc.nextInt();
+        System.out.println("chem marks : " + chem);
+        int math = sc.nextInt();
+        System.out.println("math marks : " + math);
+        System.out.println("Total " + ((phy + chem + math)/2) );
+        System.out.println( "length of array " + number.length);
+
     }
 }
--- Array/Day2.java
+++ Array/Day2.java
@@ -1,6 +1,18 @@
 public class Day2 {
+    public static void update(int marks[]){
+        for(int i=0; i<marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+    }
         public static void main(String[] args) {
-        // Your code here
-        System.out.println("day2");
+
+        //FUNCTION ARGUMENT
+        int marks[] = {97,87,25};
+        update(marks);
+        for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i] + " ");
+        }
+        System.out.println();
+        
     }
 }
