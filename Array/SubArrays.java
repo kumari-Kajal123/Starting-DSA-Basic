7bd4555521e404a8934626c0b26c2ac5313f0d3e
kumari-kajal123 <kumarikajal.2223k@gmail.com>
2026-04-30 09:00:15 +0530

    sub Arrays

--- Array/SubArrays.java
+++ Array/SubArrays.java
@@ -1,13 +1,14 @@
 public class SubArrays {
     public static void main(String[] args) {
         int arr[] = { 2, 4, 6, 8, 10, 12 };
-        for(int i=0;i<arr.length;i++){
-            int curr=arr[i];
-            for(int j=i;j<arr.length;j++){
-                System.out.print("(" + curr + "," + arr[j] + ")");
+        for(int i=0;i<arr.length;i++){           
+            for(int j=i;j<arr.length;j++){             
+                for(int k=i; k<=j;k++){    
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
             }
-        System.out.println();
-
+            System.out.println();
         }
         
     }
