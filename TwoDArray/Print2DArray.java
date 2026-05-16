59aac1abc41a7f55e30335d00c11cb6db4a81433
kumari-kajal123 <kumarikajal.2223k@gmail.com>
2026-05-16 10:12:45 +0530

    Print 2D Array

---
+++ TwoDArray/Print2DArray.java
@@ -0,0 +1,31 @@
+package TwoDArray;
+
+public class Print2DArray {
+    public static void main(String[] args) {
+    
+        int[][] arr = { {5,6,3,2} , {4,7,9,0} , {3,5,1,2} };
+
+        // APPLYING FOREACH LOOP
+        for(int[] a : arr){
+            for(int ele : a){
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("This is next method");
+
+        // USING FOR LOOP
+        int sum =0;
+        for(int j = 0 ; j< arr[0].length;j++){
+            for(int i=0 ; i< arr.length ; i++){
+                System.out.print(arr[i][j] + " ");
+                sum += arr[i][j];
+            }
+            System.out.println();
+        }
+        System.out.println(sum);
+        
+
+    }
+}
